af8e716aa35b851048c995df9ef7e225a56ae150
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-31 12:55:03 +0100

    Update helloworld

--- src/helloworld/helloworld.c
+++ src/helloworld/helloworld.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/* The code below will print the words "Hello, world!"
+to the screen, and it is amazing. */
 int main() {
-  printf("Hello World!\n");
+  // This is a comment
+  printf("Hello, world!\n");
+  printf("Have a good day!\n"); // This is also a comment
   return 0;
 }
