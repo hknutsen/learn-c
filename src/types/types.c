313c00dde537cbbfec97588b1740be5630e0a467
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-31 13:34:24 +0100

    Add types

---
+++ src/types/types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5, y = 6;
+  int sum = x + y;
+  printf("The sum of %d and %d is %d\n", x, y, sum);
+
+  float price = 4.50;
+  printf("The price of a chocolate bar is %.2f\n", price);
+
+  char letter = 'H';
+  printf("My favorite letter is %c\n", letter);
+
+  return 0;
+}
