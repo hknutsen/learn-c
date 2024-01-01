ae7adf87770f1365e782ef58855f44cba6ef8486
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-01-01 14:52:49 +0100

    Add comment

--- src/types/types.c
+++ src/types/types.c
@@ -6,7 +6,7 @@ int main() {
   printf("The sum of %d and %d is %d\n", x, y, sum);
 
   float price = 4.50;
-  printf("The price of a chocolate bar is %.2f\n", price);
+  printf("The price of a chocolate bar is %.2f\n", price); // Print price with two-decimal precision
 
   char letter = 'H';
   printf("My favorite letter is %c\n", letter);
